f6d66060f8bcc846e30066fe4c15c82d980c9d07
Gustavo Del Rosal <gustavo.delrosal5061@alumnos.udg.mx>
2026-08-26 19:57:26 -0600

    R2: prediccion de conversiones

---
+++ r02-tipos/predice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout<<7/2<<std::endl; 
+    std::cout<<7/2.0<<std::endl; 
+    std::cout<<7%2<<std::endl; 
+    std::cout<<static_cast<int>(3.99)<<std::endl; 
+    std::cout<<(1==1.0)<<std::endl; 
+    unsigned int u = 3; 
+    std::cout<<u-5<<std::endl; 
+    return 0; 
+}
